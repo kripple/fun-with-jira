e76d98017088366b1642b935c0fc8c50f6992f84
Kelly Ripple <kelly.m.ripple@gmail.com>
2016-02-03 17:06:14 -0500

    added some resource links for tomorrow

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// this is a better pattern: http://stackoverflow.com/questions/25279599/authentication-error-401-when-using-node-jira-module
+// check here to fix auth headers: https://developer.atlassian.com/jiradev/jira-apis/jira-rest-apis/jira-rest-api-tutorials/jira-rest-api-example-basic-authentication
 var request = new XMLHttpRequest();
 var url = 'https://kripple.atlassian.net/rest/api/latest/issue/MFP-1.json';
 var user = 'kelly.m.ripple@gmail.com';
@@ -12,7 +14,7 @@ request.open('GET', url, user, password);
 
 request.onload = function() {
   if (request.status >= 200 && request.status < 400) {
-    var data = JSON.parse(request.responseText);
+    var data = JSON.parse(request.responseText); // or: var resp = request.responseText;
 		console.log("Success!");
   } else {
   	console.log("We reached our target server, but it returned an error with code " + request.status); 
